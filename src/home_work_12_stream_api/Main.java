5dde44037075e2f7d07d76797053791cadf5947b
SHiCKn1 <SHiCKn1@gmail.com>
2022-12-08 18:11:33 +0300

    add 3.2

--- src/home_work_12_stream_api/Main.java
+++ src/home_work_12_stream_api/Main.java
@@ -33,22 +33,41 @@ public class Main {
         System.out.println("Сумма всех чисел: " + arrInteger.stream().mapToInt(i -> i).sum());
         System.out.println("Сумма всех цифр: " + arrInteger.stream().mapToInt(i -> i).map(i -> sumOfNumber(i)).sum());
 
-
-
+        System.out.println("Выведем начальный массив");
         List<String> myList = Arrays.asList("a1", "a2", "a3", "b1", "b3", "c2", "c1", "c5");
-        List<String> stringsWithLoginWord = myList.stream().filter(s -> !(s.contains("3"))).collect(Collectors.toList());
-        stringsWithLoginWord.forEach(System.out::println);
+        System.out.println(myList);
+        System.out.println();
 
+//        System.out.println("Выводим без элементов которые не содержат 3: ");
+//        List<String> stringsWithLoginWord = myList.stream().filter(s -> !(s.contains("3"))).collect(Collectors.toList());
+//        System.out.println(stringsWithLoginWord);
+//        System.out.println();
+
+        System.out.println("Выводим сортированный массив: ");
         Comparator<String> myStringComparator = (s1, s2) -> {
+            int num1 = Integer.parseInt(s1.replaceAll("\\D", ""));
+            int num2 = Integer.parseInt(s2.replaceAll("\\D", ""));
+            char letter1 = s1.replaceAll("\\d", "").charAt(0);
+            char letter2 = s2.replaceAll("\\d", "").charAt(0);
 
-//            if (s1.replaceAll("\\w","") > s2.replaceAll("\\w","")) {
-//                return 1;
-//            } else if (s1.replaceAll("\\w","") > s2.replaceAll("\w","")) {
-//                return -1;
-//            } else {
-                return 0;
-//            }
+            if (num1 > num2) {
+                return 1;
+            } else if (num1 < num2) {
+                return -1;
+            } else {
+                if (letter2 > letter1) {
+                    return 1;
+                } else if (letter2 < letter1) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
         };
+
+        myList = myList.stream().sorted(myStringComparator).collect(Collectors.toList());
+        System.out.println(myList);
+        System.out.println();
 
 
     }
